9807ebadffe8cb0427d5bd45ccdd617940ca4705
ShikoShintaro <shikoshintaro21@gmail.com>
2026-03-22 04:27:35 +0800

    updated userschema

--- server/models/User.js
+++ server/models/User.js
@@ -2,8 +2,28 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
     username: String,
-    email: { type: String, unique: true },
-    password: String
+    email: {
+        type: String,
+        unique: true
+    },
+    password: String,
+    otp : {
+        type: String
+    },
+    otpExpires : {
+        type: Date
+    },
+    verified : {
+        type : Boolean,
+        default : false
+    },
+    resetOtp : {
+        type : String,
+    },
+    resetOtpExpires : {
+        type : Date
+    }
+
 });
 
 module.exports = mongoose.model("User", UserSchema);
