de2df5db5a6c6d999bf940e612b1e4a1b52405bb
ShikoShintaro <shikoshintaro21@gmail.com>
2026-03-27 04:12:07 +0800

    updated Retrofit fixing Adding OkHttp3 client

--- app/src/main/java/com/olokogini/moriai/api/RetroFitClient.kt
+++ app/src/main/java/com/olokogini/moriai/api/RetroFitClient.kt
@@ -2,12 +2,24 @@ package com.olokogini.moriai.api
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 
 object RetroFitClient {
     private const val BASE_URL = "http://10.0.2.2:3000/api/"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
     val api : AuthService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(AuthService::class.java)
