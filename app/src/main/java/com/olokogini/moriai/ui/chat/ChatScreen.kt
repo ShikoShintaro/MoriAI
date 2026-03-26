6ab6cb154ac829ca0fa1eb49f8e587e74ae5cf5f
ShikoShintaro <shikoshintaro21@gmail.com>
2026-03-27 04:11:04 +0800

    Updated ChatScreen ( NEW SOON )

--- app/src/main/java/com/olokogini/moriai/ui/chat/ChatScreen.kt
+++ app/src/main/java/com/olokogini/moriai/ui/chat/ChatScreen.kt
@@ -1,9 +1,17 @@
 package com.olokogini.moriai.ui.chat
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.*
+
 
 @Composable
 fun ChatScreen() {
-    Text("MORI CHAT COMING SOON!")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Welcome to Mori AI!")
+    }
 }
