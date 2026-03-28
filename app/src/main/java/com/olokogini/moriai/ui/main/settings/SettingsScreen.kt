3e3fd8648f0bce7c36c8b88f84b1f012e4f313c2
ShikoShintaro <shikoshintaro21@gmail.com>
2026-03-28 21:08:37 +0800

    Update: New File SettingsScreen.kt

---
+++ app/src/main/java/com/olokogini/moriai/ui/main/settings/SettingsScreen.kt
@@ -0,0 +1,9 @@
+package com.olokogini.moriai.ui.main.settings
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingsScreen() {
+    Text("Settings Screen")
+}
