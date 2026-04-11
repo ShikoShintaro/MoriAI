2713ceb5dba1db7d5431d2c796f811adc3f0df35
ShikoShintaro <shikoshintaro21@gmail.com>
2026-04-12 03:38:13 +0800

    Update : MainContent.kt Updated

--- app/src/main/java/com/olokogini/moriai/ui/main/MainContent.kt
+++ app/src/main/java/com/olokogini/moriai/ui/main/MainContent.kt
@@ -1,9 +1,16 @@
 package com.olokogini.moriai.ui.main
 
 import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.*
 import androidx.navigation.compose.*
+import androidx.room.Room
 import com.olokogini.moriai.ui.main.chat.ChatScreen
+import com.olokogini.moriai.ui.main.chat.data.ChatDatabase
+import com.olokogini.moriai.ui.main.chat.data.ChatRepository
+import com.olokogini.moriai.ui.main.chat.viewmodel.ChatViewModel
+import com.olokogini.moriai.ui.main.chat.viewmodel.ChatViewModelFactory
 import com.olokogini.moriai.ui.main.event.EventsScreen
 import com.olokogini.moriai.ui.main.profile.ProfileScreen
 import com.olokogini.moriai.ui.main.settings.SettingsScreen
@@ -18,14 +25,31 @@ fun MainContent(innerNavController: NavHostController) {
         startDestination = "chat"
     ) {
         composable("chat") {
-            ChatScreen()
+            val context = LocalContext.current
+
+            val db = Room.databaseBuilder(
+                context.applicationContext,
+                ChatDatabase::class.java,
+                "chat_db"
+            ).build()
+
+            val repo = ChatRepository(db.chatDao())
+
+            val viewModel: ChatViewModel = viewModel(
+                factory = ChatViewModelFactory(repo)
+            )
+
+            ChatScreen(viewModel)
         }
+
         composable("profile") {
             ProfileScreen()
         }
+
         composable("settings") {
             SettingsScreen()
         }
+
 
         composable("events"){
             EventsScreen()
