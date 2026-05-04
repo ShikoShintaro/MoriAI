b6c094914026013d1a114caaa01762cfa96dd20c
ShikoShintaro <shikoshintaro21@gmail.com>
2026-05-04 16:31:10 +0800

    Update : ChatMEntity.kt fix

--- app/src/main/java/com/olokogini/moriai/ui/main/chat/data/ChatMessageEntity.kt
+++ app/src/main/java/com/olokogini/moriai/ui/main/chat/data/ChatMessageEntity.kt
@@ -1,4 +1,14 @@
 package com.olokogini.moriai.ui.main.chat.data
 
-class ChatMessageEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "chat_message")
+data class ChatMessageEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+
+    val message: String,
+    val isUser: Boolean,
+    val timestamp: Long
+)
