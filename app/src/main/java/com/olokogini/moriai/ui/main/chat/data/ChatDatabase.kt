6d9b49d537e421943122b93fefcd75bd7a2d3550
ShikoShintaro <shikoshintaro21@gmail.com>
2026-05-04 16:30:48 +0800

    Update : DhatDB.kt fix

--- app/src/main/java/com/olokogini/moriai/ui/main/chat/data/ChatDatabase.kt
+++ app/src/main/java/com/olokogini/moriai/ui/main/chat/data/ChatDatabase.kt
@@ -1,4 +1,13 @@
 package com.olokogini.moriai.ui.main.chat.data
 
-class ChatDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ChatMessageEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class ChatDatabase : RoomDatabase() {
+    abstract fun chatDao(): ChatDao
 }
