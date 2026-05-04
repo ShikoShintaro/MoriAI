f7742a4e2ef64bccb8772f3dcfcc88e8634ec668
ShikoShintaro <shikoshintaro21@gmail.com>
2026-05-04 16:30:38 +0800

    Update : ChatDao.kt fix

--- app/src/main/java/com/olokogini/moriai/ui/main/chat/data/ChatDao.kt
+++ app/src/main/java/com/olokogini/moriai/ui/main/chat/data/ChatDao.kt
@@ -1,2 +1,20 @@
 package com.olokogini.moriai.ui.main.chat.data
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ChatDao {
+
+    @Query("SELECT * FROM chat_message ORDER BY timestamp ASC")
+    fun getMessages(): Flow<List<ChatMessageEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMessage(message: ChatMessageEntity)
+
+    @Query("DELETE FROM chat_message")
+    suspend fun clearChat()
+}
